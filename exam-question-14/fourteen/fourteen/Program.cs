1e05217ae5d80302d2744efbeea5834aa48cb2e4
peymancyb <paymanghazvini@yahoo.com>
2018-01-20 17:20:22 +0400

    solved!

--- exam-question-14/fourteen/fourteen/Program.cs
+++ exam-question-14/fourteen/fourteen/Program.cs
@@ -3,32 +3,49 @@
 //with one integer parameter, and which has been declared and initialized String type array with 10 values.In method f() need find the values of array, that length are bigger "k" parameter and print these values.In inherited class  is declared int k variable, which initialized by constructor of derived class, and inside of this class declared method by name show() { }
 //that prints sum of k variable from base class and variable with the same name of inherited class.
 
+
+//can not understand this part of the question!!!
+//***that prints sum of k variable from base class and variable with the same name of inherited class.***
+
 using System;
 
 namespace fourteen
 {
     //base class
-    class BaseClass{
+    public class BaseClass{
         public int k;
         //constructor
         public BaseClass(int firstArg)
         {
             //initializing k by firstArg
-            this.k = firstArg;
+            k = firstArg;
         }
         public void f(int d){ 
             //empty
             string[] items = new string[10]{
                 "one","two","three","four","five","six","seven","eight","nine","ten"
             };
-
-            for (int i = 0; i < 10; i++){
-                if(i > d){
+            if(d>10 || d<0){
+                Console.WriteLine("not correct index!");
+            }else{
+            for (int i = d; i > this.k; i++)
+                {
                     Console.WriteLine(items[i]);
                 }
             }
-
         }
+    }
+
+    public class DerivedClass: BaseClass {
+        //constructor
+        //public int num;
+        public DerivedClass(int num):base(firstArg:num){
+            //empty
+        }
+        public void show(){
+            Console.WriteLine("sum of base class");
+        }
+ 
     }
 
 
@@ -37,9 +54,11 @@ namespace fourteen
     {
         public static void Main(string[] args)
         {
-            //MainClass answer = new MainClass(10);
-            //Console.WriteLine(answer.k);
             Console.WriteLine("Hello World!");
+            //set k to 0
+            DerivedClass ob = new DerivedClass(1);
+            ob.f(9);
+            ob.show();
         }
     }
 }
